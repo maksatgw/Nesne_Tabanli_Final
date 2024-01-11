a0b8b9d9b97f44dcd676db8851773197b1cfee19
maksatgw <garyagdyyev@outlook.com>
2024-01-11 23:15:19 +0300

    Ders 10 - 2

---
+++ OOP_10Hafta_2/ClassLibrary2/Ortalama_Hesapla.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Ortalama_Hesapla
+    {
+        //Ben geçti kaldı karar döngüsünü metodun içinde ayarladım
+        //İsteyen int değer döndürüp form içinde sağlamasını yapabilir.
+        public string Hesapla(int vize1, int vize2, int final)
+        {
+            int ortalama = (vize1 + vize2 + final) / 3;
+
+            if (ortalama < 50)
+            {
+                return "Kaldı";
+            }
+            else
+            {
+                return "Geçti";
+            }
+        }
+    }
+}
---
+++ OOP_10Hafta_2/OrtalamaHesaplayici/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//Adım 1 
+using ClassLibrary2;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrtalamaHesaplayici
+{
+    public partial class Form1 : Form
+    {
+        /*
+            Öğrencilerin vize1 ,vize2 ve 
+            final notları klavyeden girildikten 
+            sonra sonucu ekranda görüntüleyen program kodunu yazınız. 
+            Koşul, öğrencilerin notları textbox1, textbox2, ve textbox3 ten 
+            girilecek sonuç textbox4 te 
+            not ortalaması ile birlikte geçti ve kaldı olarak gözükecek
+         */
+
+        //Adım 2
+        Ortalama_Hesapla hesap = new Ortalama_Hesapla();
+        int a, b, c;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Adım 3
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = Convert.ToInt32(textBox3.Text);
+
+            textBox4.Text = hesap.Hesapla(a, b, c);
+        }
+    }
+}
