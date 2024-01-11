5f19d70048b5b9e770378b36eb0c81821ae188ab
maksatgw <garyagdyyev@outlook.com>
2024-01-11 20:55:24 +0300

    Ders 6 - 3

---
+++ OOP_6Hafta_3/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace OOP_6Hafta_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.OGRNO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OKULISMI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.YIL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 303);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(198, 92);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(158, 303);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(385, 92);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(158, 303);
+            this.listBox3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OGRNO,
+            this.OKULISMI,
+            this.YIL});
+            this.dataGridView1.Location = new System.Drawing.Point(549, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(466, 303);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(371, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OGRNO
+            // 
+            this.OGRNO.HeaderText = "OGRNO";
+            this.OGRNO.Name = "OGRNO";
+            // 
+            // OKULISMI
+            // 
+            this.OKULISMI.HeaderText = "OKULISMI";
+            this.OKULISMI.Name = "OKULISMI";
+            // 
+            // YIL
+            // 
+            this.YIL.HeaderText = "YIL";
+            this.YIL.Name = "YIL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 524);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OGRNO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OKULISMI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn YIL;
+    }
+}
+
---
+++ OOP_6Hafta_3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//[1] Threads Kütüphanesini ekliyoruz
+using System.Threading;
+
+namespace OOP_6Hafta_3
+{
+    public partial class Form1 : Form
+    {
+        /*
+         Form üzerine 3 adet listbox, 1 adet DataGridView ve 1 adet button yerleştirin.
+         Form load edildiği zaman ListBox1 içinde öğrenci numaraları (başlangıç noktası 2023000001 olmak kaydıyla 1'er 1'er artarak devam edecek.
+         Bununla aynı anda bölüm ismi Listbox2'de, yıl ListBox3'te görüntülenecek. 
+         listBox 1 değerleri DataGridView kolon 1'de, Listbox3 Kolon 2'de, Listbox3 de kolon3 de aynı anda görüntülenecek.
+         Not: Numara 1'er artarken yıl ve bölüm değerleri sabit kalacak.
+        */
+        //Sırası ile çözümleri [] parantezlerin içinde sayıyla belirledim.
+
+        //[1] Thread nesnelerini tanımlıyoruz
+        Thread task1;
+        Thread task2;
+        Thread task3;
+        Thread task4;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //[2]Böylelikle metot otomatik olarak formun load olayına bağlıdır
+            //Mutlaka yazmak zorundayiz
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            
+            //[6] Tanımladığımız tasklare ThreadStart ile çalıştıracağımız metotları veriyoruz, ardından start ile başlatıyoruz.
+            task1 = new Thread(new ThreadStart(OgrenciNumaralari));
+            task2 = new Thread(new ThreadStart(func2));
+            task3 = new Thread(new ThreadStart(func3));
+            task4 = new Thread(new ThreadStart(func4));
+            task1.Start();
+            task2.Start();
+            task3.Start();
+            task4.Start();
+        }
+
+        //[3] Öğrenci numalaraları için bir adet numara ve bir adet counter değişkeni tanımlıyoruz.
+        int no = 2023000001;
+        //Counter değişkenimizin değerini -1 yapıyoruz
+        //Yani ilk değerin 0 olması lazım. Threadler ile eşzamanlı çalıştığımız için bu değere 0 atarsak, OgrenciNumaralari fonksiyonundan sonra 1 olacagi icin
+        //Program hata verir. Indexler, 0dan başlar.
+        //Listbox icindeki nesneleri ayni anda hem yazdirip hem eklersek 1 numarali index yazilmadan direkt acmaya calisacagi icin hata verecektir.
+        int counter = -1;
+
+
+        //[4] Formload içine yazmaktansa ayrı metodlar oluşturuyoruz.
+        void OgrenciNumaralari()
+        {
+            //Sonsuz bir döngü oluşturmak için while true döngüsü oluşturuyoruz.
+            while (true)
+            {
+                //Listeye ilk numarayı ekledik, bir arttırdık.
+                listBox1.Items.Add(no);
+                no++;
+                //Counter nesnemizi işte burada sıfır yaptık. neden sıfır? INDEXLER SIFIRDAN BAŞLAR DA ONDAN.
+                counter++;
+                Thread.Sleep(1000);
+            }
+        }
+        void func2()
+        {
+            while (true)
+            {
+                listBox2.Items.Add(DateTime.Now.Year);
+                Thread.Sleep(1000);
+            }
+        }
+        void func3()
+        {
+            while (true)
+            {
+                listBox3.Items.Add("D.E.Ü. İ.M.Y.O. Bilgisayar");
+                Thread.Sleep(1000);
+            }
+        }
+
+        void func4()
+        {
+            Thread.Sleep(100);
+            while (true)
+            {
+                //DataGridView nesnesinin ilgili satirinin birinca kolonuna sirasiyla
+                //Listbox1.Items[0],1,2 diye giderek eklemeler yapiyoruz.
+                dataGridView1.Rows.Add(listBox1.Items[counter], listBox2.Items[counter], listBox3.Items[counter]);
+                Thread.Sleep(1000);
+            }
+        }
+        //[5] Butona bastigimizda, gorevleri bitiriyoruz. 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            task1.Abort();
+            task2.Abort();
+            task3.Abort();
+            task4.Abort();
+
+        }
+
+    }
+}   
