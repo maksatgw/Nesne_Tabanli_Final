3c5a27070315d8a38869b28090df078396ba3b08
maksatgw <garyagdyyev@outlook.com>
2024-01-11 23:51:50 +0300

    Ders 10 - 3 (Hoca Kodu Değil)

---
+++ OOP_10Hafta_3/ClassLibrary3/Personel_Maas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    public class Personel_Maas
+    {
+        public int MaasHesapla(int brut, int mesai, int cocuk)
+        {
+            int vergi = brut * 14 / 100;
+            int gunluk = brut / 30;
+            int saatlik = gunluk / 8;
+            int cocukYardim = (brut * 2 / 100) * cocuk;
+            int mesailiUcret = saatlik * mesai;    
+            int net = cocukYardim + mesailiUcret + brut - vergi;
+            return net;
+        }
+    }
+}
